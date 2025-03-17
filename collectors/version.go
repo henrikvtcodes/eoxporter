dffb93fc4dbf91ca66b3da2d57f0c54300101ea0
Henrik VT <commits@henrikvt.com>
2025-03-17 17:46:19 -0400

    version info collector

--- collectors/version.go
+++ collectors/version.go
@@ -1,1 +1,21 @@
 package collectors
+
+type VersionCollector struct {
+	Uptime           float64 `json:"uptime"`
+	ModelName        string  `json:"modelName"`
+	InternalVersion  string  `json:"internalVersion"`
+	SystemMacAddress string  `json:"systemMacAddress"`
+	SerialNumber     string  `json:"serialNumber"`
+	BootupTimestamp  int64   `json:"bootupTimestamp"`
+	MemTotal         int64   `json:"memTotal"`
+	MemFree          int64   `json:"memFree"`
+	Version          string  `json:"version"`
+	Architecture     string  `json:"architecture"`
+	IsIntlVersion    bool    `json:"isIntlVersion"`
+	InternalBuildId  string  `json:"internalBuildId"`
+	HardwareRevision string  `json:"hardwareRevision"`
+}
+
+func (s *VersionCollector) GetCmd() string {
+	return "show version"
+}
