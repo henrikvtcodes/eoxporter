f70257250c9444f31835b4de2bca9aed07600509
Henrik VT <commits@henrikvt.com>
2025-03-17 17:58:42 -0400

    working demo stuff

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/aristanetworks/goeapi"
+	"github.com/henrikvtcodes/eoxporter/collectors"
+)
 
+func main() {
+	fmt.Printf("Connections:%s\n", goeapi.Connections())
+	node, err := goeapi.ConnectTo("hudson")
+	if err != nil {
+		panic(err)
+	}
+
+	eapiVersion := &collectors.VersionCollector{}
+
+	handle, _ := node.GetHandle("json")
+	handle.AddCommand(eapiVersion)
+
+	if err := handle.Call(); err != nil {
+		panic(err)
+	}
+
+	println(eapiVersion.Version)
 }
