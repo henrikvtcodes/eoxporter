00db3d41f91c1118324fd0dd94e98267cf6087ff
Henrik VT <commits@henrikvt.com>
2025-03-18 14:42:25 -0400

    collectors: add interfaces collector

--- collectors/interfaces.go
+++ collectors/interfaces.go
@@ -1,1 +1,25 @@
 package collectors
+
+type InterfacesCollector struct {
+	Interfaces map[string]Interface `json:"interfaces"`
+}
+
+type Interface struct {
+	OutBroadcastPackets int     `json:"outBroadcastPkts"`
+	OutUnicastPackets   int     `json:"outUcastPkts"`
+	OutMulticastPackets int     `json:"outMulticastPkts"`
+	OutDiscards         int     `json:"outDiscards"`
+	OutOctets           int     `json:"outOctets"`
+	InBroadcastPackets  int     `json:"inBroadcastPkts"`
+	InUnicastPackets    int     `json:"inUcastPkts"`
+	InMulticastPackets  int     `json:"inMulticastPkts"`
+	InDiscards          int     `json:"inDiscards"`
+	InOctets            int     `json:"inOctets"`
+	LastUpdateTimestamp float64 `json:"lastUpdateTimestamp"`
+}
+
+func (c *InterfacesCollector) GetCmd() string {
+	// In the context of eAPI, this command seems to return the output of "show interfaces counters incoming"
+	// and show interfaces counters outgoing" (and the outgoing command does the same)
+	return "show interfaces counters incoming"
+}
