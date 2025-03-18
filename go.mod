5fbf05704534c799cf169d13027bc4a9e0f4daed
Henrik VT <commits@henrikvt.com>
2025-03-18 17:04:50 -0400

    base http stuff

--- go.mod
+++ go.mod
@@ -4,6 +4,18 @@ go 1.23.2
 
 require (
 	github.com/aristanetworks/goeapi v1.0.0
+	github.com/prometheus/client_golang v1.21.1
+)
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.62.0 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
 	github.com/vaughan0/go-ini v0.0.0-20130923145212-a98ad7ee00ec // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	google.golang.org/protobuf v1.36.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,26 +1,45 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/aristanetworks/goeapi"
-	"github.com/henrikvtcodes/eoxporter/collectors"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const DefaultCollectors = "version,cooling,power,temperature"
+
+var (
+	eapiConfigPath           = flag.String("eapi-config", os.Getenv("EAPI_CONF"), "Path to Arista eAPI config file")
+	defaultCollectorsEnabled = flag.String("collectors", DefaultCollectors, "Comma-separated list of collectors to enable")
+	listenAddress            = flag.String("listen-address", "0.0.0.0:9396", "Address to listen on for HTTP")
 )
 
 func main() {
-	fmt.Printf("Connections:%s\n", goeapi.Connections())
-	node, err := goeapi.ConnectTo("hudson")
+	flag.Parse()
+
+	// Handle eAPI Config Path Loading
+	eapiAbsoluteConfigPath, err := filepath.Abs(*eapiConfigPath)
 	if err != nil {
+		println(fmt.Errorf("invalid eapiConfig path: %s", eapiAbsoluteConfigPath))
 		panic(err)
 	}
+	goeapi.LoadConfig(eapiAbsoluteConfigPath)
 
-	eapiVersion := &collectors.VersionCollector{}
+	// Handle enabled eAPI Collectors
+	defaultCollectors := makeCollectors(strings.Split(*defaultCollectorsEnabled, ","))
 
-	handle, _ := node.GetHandle("json")
-	handle.AddCommand(eapiVersion)
+	http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		MetricsHandler(w, r, &defaultCollectors)
+	})
 
-	if err := handle.Call(); err != nil {
-		panic(err)
+	// Handle HTTP requests
+	servErr := http.ListenAndServe(*listenAddress, nil)
+	if servErr != nil {
+		log.Default().Fatalf("[FATAL] HTTP Server Error: %s", err)
 	}
-
-	println(eapiVersion.Version)
 }
