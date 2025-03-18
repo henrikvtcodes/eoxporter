147c10e4a6aa758af6ff7c99d88d3662c9d3ffba
Henrik VT <commits@henrikvt.com>
2025-03-18 17:35:52 -0400

    experiments with package builds

--- eoxporter.nix
+++ eoxporter.nix
@@ -1,6 +1,7 @@
 {
   lib,
   buildGoModule,
+  pkgs,
   ...
 }:
 buildGoModule rec {
@@ -8,6 +9,12 @@ buildGoModule rec {
   version = "0.0.0";
 
   src = ./.;
+  doCheck = false;
+
+#  preBuild = ''
+#  export GOPROXY='https://proxy.golang.org,direct'
+#  ${pkgs.go}/bin/go mod vendor
+#  '';
 
   vendorHash = "sha256-R7dBP5kR+oRypF2ppCF5CrZiqNzKmQZPxQ3ycQbxjq0=";
   # vendorHash = lib.fakeHash;
