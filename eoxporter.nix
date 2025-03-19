60183754f889e3e7ee5a429bd2ae91902677fe0c
Henrik VT <commits@henrikvt.com>
2025-03-19 12:12:41 -0400

    migrate to gomod2nix

--- eoxporter.nix
+++ eoxporter.nix
@@ -1,24 +1,15 @@
 {
   lib,
   buildGoModule,
+  buildGoApplication,
   pkgs,
   ...
 }:
-buildGoModule rec {
+buildGoApplication {
   pname = "eoxporter";
   version = "0.0.0";
 
   src = ./.;
-  doCheck = false;
-  proxyVendor = true;
-
-  preBuild = ''
-    export GOPROXY='https://proxy.golang.org,direct'
-    ${pkgs.go}/bin/go mod tidy
-    ${pkgs.go}/bin/go mod vendor
-  '';
-
-#  vendorHash = "sha256-R7dBP5kR+oRypF2ppCF5CrZiqNzKmQZPxQ3ycQbxjq0=";
-vendorHash = null;  
-  # vendorHash = lib.fakeHash;
+  pwd = ./.;
+  modules = ./gomod2nix.toml;
 }
--- flake.nix
+++ flake.nix
@@ -5,10 +5,17 @@
     nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
+
+    gomod2nix = {
+      url = "github:nix-community/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ {
     flake-parts,
+    nixpkgs,
+    gomod2nix,
     ...
   }:
     flake-parts.lib.mkFlake {inherit inputs;} {
@@ -22,8 +29,20 @@
         ...
       }: {
         packages = rec {
-          default = pkgs.callPackage ./eoxporter.nix {};
+          default = pkgs.callPackage ./eoxporter.nix {
+            inherit (gomod2nix.legacyPackages.${system}) buildGoApplication;
+          };
           eoxporter = default;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            gopls
+            gotools
+            go-tools
+            gomod2nix.packages.${system}.default
+          ];
         };
         formatter = pkgs.alejandra;
       };
