557e1d7a9ae579a554e05d5c992e631f37ba5531
Henrik VT <commits@henrikvt.com>
2025-03-19 00:05:07 -0400

    impl metrics for cooling and temperature collectors

--- collectors/cooling.go
+++ collectors/cooling.go
@@ -1,6 +1,9 @@
 package collectors
 
-import "github.com/prometheus/client_golang/prometheus"
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"strconv"
+)
 
 type CoolingCollector struct {
 	OverrideFanSpeed           int       `json:"overrideFanSpeed"`
@@ -11,6 +14,13 @@ type CoolingCollector struct {
 	AirflowDirection           string    `json:"airflowDirection"`
 	PowerSupplySlots           []FanSlot `json:"powerSupplySlots"`
 	FanTraySlots               []FanSlot `json:"fanTraySlots"`
+
+	coolingMeta             *prometheus.GaugeVec
+	ambientTemperature      *prometheus.GaugeVec
+	overrideFanSpeedGauge   *prometheus.GaugeVec
+	fanMaxSpeedGauge        *prometheus.GaugeVec
+	fanConfiguredSpeedGauge *prometheus.GaugeVec
+	fanActualSpeedGauge     *prometheus.GaugeVec
 }
 
 type FanSlot struct {
@@ -28,16 +38,46 @@ type FanStatus struct {
 	ActualSpeed               int     `json:"actualSpeed"`
 	SpeedStable               bool    `json:"speedStable"`
 	LastSpeedStableChangeTime float64 `json:"lastSpeedStableChangeTime"`
+	Label                     string  `json:"label"`
 }
 
 func (c *CoolingCollector) GetCmd() string {
 	return "show environment cooling"
 }
 
-func (c *CoolingCollector) Register(registry *prometheus.Registry) {
+var coolingOpts = MakeSubsystemOptsFactory("cooling")
 
+func (c *CoolingCollector) Register(registry *prometheus.Registry) {
+	c.coolingMeta = prometheus.NewGaugeVec(coolingOpts("meta", "Metric containing meta information about the device's cooling system/config"),
+		[]string{"coolingMode", "shutdownOnInsufficientFans", "systemStatus"})
+	c.ambientTemperature = prometheus.NewGaugeVec(coolingOpts("ambient_temperature", "Ambient Temperature in Celsius"), []string{})
+	c.overrideFanSpeedGauge = prometheus.NewGaugeVec(coolingOpts("override_fan_speed", "Fan speed override. If 0, the fan speed is not currently overridden"), []string{})
+
+	labels := []string{"tray", "fanName"}
+	c.fanMaxSpeedGauge = prometheus.NewGaugeVec(coolingOpts("fan_max_speed", "Maximum capable fan speed"), labels)
+	c.fanConfiguredSpeedGauge = prometheus.NewGaugeVec(coolingOpts("fan_configured_speed", "Configured speed"), labels)
+	c.fanActualSpeedGauge = prometheus.NewGaugeVec(coolingOpts("fan_actual_speed", "Actual speed"), labels)
+	registry.MustRegister(c.fanMaxSpeedGauge)
 }
 
 func (c *CoolingCollector) UpdateMetrics() {
+	c.coolingMeta.WithLabelValues(c.CoolingMode, strconv.FormatBool(c.ShutdownOnInsufficientFans), c.SystemStatus).Set(1)
+	c.ambientTemperature.WithLabelValues().Set(c.AmbientTemperature)
+	c.overrideFanSpeedGauge.WithLabelValues().Set(float64(c.OverrideFanSpeed))
 
+	for _, slot := range c.PowerSupplySlots {
+		for _, fan := range slot.Fans {
+			c.fanMaxSpeedGauge.WithLabelValues("psu", fan.Label).Set(float64(fan.MaxSpeed))
+			c.fanConfiguredSpeedGauge.WithLabelValues("psu", fan.Label).Set(float64(fan.ConfiguredSpeed))
+			c.fanActualSpeedGauge.WithLabelValues("psu", fan.Label).Set(float64(fan.ActualSpeed))
+		}
+	}
+
+	for _, slot := range c.FanTraySlots {
+		for _, fan := range slot.Fans {
+			c.fanMaxSpeedGauge.WithLabelValues("fanslot", fan.Label).Set(float64(fan.MaxSpeed))
+			c.fanConfiguredSpeedGauge.WithLabelValues("fanslot", fan.Label).Set(float64(fan.ConfiguredSpeed))
+			c.fanActualSpeedGauge.WithLabelValues("fanslot", fan.Label).Set(float64(fan.ActualSpeed))
+		}
+	}
 }
--- collectors/temperature.go
+++ collectors/temperature.go
@@ -7,6 +7,13 @@ type TemperatureCollector struct {
 	SystemStatus       string              `json:"systemStatus"`
 	TemperatureSensors []TemperatureSensor `json:"tempSensors"`
 	PowerSupplySlots   []PSUSlot           `json:"powerSupplySlots"`
+
+	maxTempGauge               *prometheus.GaugeVec
+	tempAlertCount             *prometheus.GaugeVec
+	overheatThresholdTempGauge *prometheus.GaugeVec
+	criticalThresholdTempGauge *prometheus.GaugeVec
+	targetTempGauge            *prometheus.GaugeVec
+	currentTempGauge           *prometheus.GaugeVec
 }
 
 type PSUSlot struct {
@@ -36,10 +43,40 @@ func (c *TemperatureCollector) GetCmd() string {
 	return "show environment temperature"
 }
 
-func (c *TemperatureCollector) Register(registry *prometheus.Registry) {
+var tempOpts = MakeSubsystemOptsFactory("temperature")
 
+func (c *TemperatureCollector) Register(registry *prometheus.Registry) {
+	labels := []string{"sensorName", "sensorDescription"}
+	c.maxTempGauge = prometheus.NewGaugeVec(tempOpts("max_temp", "The highest temperature that this sensor has hit"), labels)
+	c.tempAlertCount = prometheus.NewGaugeVec(tempOpts("temp_alert", "Temperature Sensor Alert Count"), labels)
+	c.overheatThresholdTempGauge = prometheus.NewGaugeVec(tempOpts("overheat_threshold", "Overheat Temperature Threshold"), labels)
+	c.criticalThresholdTempGauge = prometheus.NewGaugeVec(tempOpts("critical_threshold", "Critical Temperature Threshold"), labels)
+	c.targetTempGauge = prometheus.NewGaugeVec(tempOpts("target_temp", "Target Temperature"), labels)
+	c.currentTempGauge = prometheus.NewGaugeVec(tempOpts("current_temp", "Current Temperature"), labels)
+
+	registry.MustRegister(c.maxTempGauge, c.tempAlertCount, c.overheatThresholdTempGauge, c.criticalThresholdTempGauge, c.targetTempGauge, c.currentTempGauge)
 }
 
 func (c *TemperatureCollector) UpdateMetrics() {
+	for _, sensor := range c.TemperatureSensors {
+		labels := []string{sensor.Name, sensor.Description}
+		c.maxTempGauge.WithLabelValues(labels...).Set(sensor.MaxTemperature)
+		c.tempAlertCount.WithLabelValues(labels...).Set(float64(sensor.AlertCount))
+		c.overheatThresholdTempGauge.WithLabelValues(labels...).Set(float64(sensor.OverheatThreshold))
+		c.criticalThresholdTempGauge.WithLabelValues(labels...).Set(float64(sensor.CriticalThreshold))
+		c.targetTempGauge.WithLabelValues(labels...).Set(float64(sensor.TargetTemperature))
+		c.currentTempGauge.WithLabelValues(labels...).Set(float64(sensor.CurrentTemperature))
+	}
 
+	for _, psu := range c.PowerSupplySlots {
+		for _, sensor := range psu.TemperatureSensors {
+			labels := []string{sensor.Name, sensor.Description}
+			c.maxTempGauge.WithLabelValues(labels...).Set(sensor.MaxTemperature)
+			c.tempAlertCount.WithLabelValues(labels...).Set(float64(sensor.AlertCount))
+			c.overheatThresholdTempGauge.WithLabelValues(labels...).Set(float64(sensor.OverheatThreshold))
+			c.criticalThresholdTempGauge.WithLabelValues(labels...).Set(float64(sensor.CriticalThreshold))
+			c.targetTempGauge.WithLabelValues(labels...).Set(float64(sensor.TargetTemperature))
+			c.currentTempGauge.WithLabelValues(labels...).Set(float64(sensor.CurrentTemperature))
+		}
+	}
 }
